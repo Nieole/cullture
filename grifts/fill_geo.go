8394abdc34819d7020d1eeee7c3ced48bceb3a98
nicoer <saber00sword@google.com>
2020-03-28 11:06:26 +0800

    change geo name to id

--- grifts/fill_geo.go
+++ grifts/fill_geo.go
@@ -2,6 +2,7 @@ package grifts
 
 import (
 	"culture/models"
+	"fmt"
 	"log"
 
 	"github.com/go-redis/redis/v7"
@@ -13,15 +14,16 @@ var _ = Namespace("project", func() {
 	Desc("fill_geo", "Task Description")
 	Add("fill_geo", func(c *Context) error {
 		projects := &models.Projects{}
-		if err := models.DB.Select("name").All(projects); err != nil {
+		if err := models.DB.Select("id").All(projects); err != nil {
 			log.Fatalf("quert all project failed : %v", err)
 		}
 		geos := make([]*redis.GeoLocation, 0, len(*projects))
 		for _, project := range *projects {
 			geos = append(geos, &redis.GeoLocation{
-				Name: project.Name,
+				Name: project.ID.String(),
 			})
 		}
+		fmt.Println(geos)
 		models.REDIS.GeoAdd("project_geo", geos...)
 		return nil
 	})
