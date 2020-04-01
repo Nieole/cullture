0978a657aa02d84f2954b2392602345c62c335c9
nicoer <saber00sword@google.com>
2020-04-01 14:07:44 +0800

    fie cache

--- actions/posts.go
+++ actions/posts.go
@@ -55,14 +55,14 @@ func (v PostsResource) List(c buffalo.Context) error {
 	// Default values are "page=1" and "per_page=20".
 	q := tx.PaginateFromParams(c.Params())
 
-	result, err := models.REDIS.Get(fmt.Sprintf("cache:%v", c.Param("updated_at"))).Result()
+	result, err := models.REDIS.Get(fmt.Sprintf("cache:%v:%v", c.Param("updated_at"), c.Param("project_id"))).Result()
 	if err != nil {
 		mu.Lock()
 		// Retrieve all Posts from the DB
 		if err := q.Eager("Tags", "Project").Scope(ByPage(c.Param("updated_at"))).Scope(ByProject(c.Param("project_id"))).Where("is_delete = ?", false).Order("updated_at desc").All(posts); err != nil {
 			return err
 		}
-		models.REDIS.Set(fmt.Sprintf("cache:%v", c.Param("updated_at")), posts.String(), time.Second*3)
+		models.REDIS.Set(fmt.Sprintf("cache:%v:%v", c.Param("updated_at"), c.Param("project_id")), posts.String(), time.Second*3)
 		mu.Unlock()
 	} else {
 		err := posts.FromString(result)
@@ -121,13 +121,13 @@ func MyList(c buffalo.Context) error {
 		return c.Render(http.StatusBadRequest, Fail(err.Error()))
 	}
 
-	result, err := models.REDIS.Get(fmt.Sprintf("cache:my:%v:%v", c.Param("updated_at"), phone)).Result()
+	result, err := models.REDIS.Get(fmt.Sprintf("cache:my:%v:%v:%v", c.Param("updated_at"), c.Param("project_id"), phone)).Result()
 	if err != nil {
 		// Retrieve all Posts from the DB
 		if err := q.Eager("Tags", "Project").Scope(ByPage(c.Param("updated_at"))).Scope(ByProject(c.Param("project_id"))).Where("user_phone = ?", phone).Where("is_delete = ?", false).Order("updated_at desc").All(posts); err != nil {
 			return err
 		}
-		models.REDIS.Set(fmt.Sprintf("cache:my:%v", c.Param("updated_at")), posts.String(), time.Second*3)
+		models.REDIS.Set(fmt.Sprintf("cache:my:%v:%v:%v", c.Param("updated_at"), c.Param("project_id"), phone), posts.String(), time.Second*3)
 	} else {
 		err := posts.FromString(result)
 		if err != nil {
