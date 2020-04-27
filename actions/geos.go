cd42bed699225f0c00759dbe0454048a99bc8413
nicoer <saber00sword@gmail.com>
2020-04-27 10:07:56 +0800

    add geo cache

--- actions/geos.go
+++ actions/geos.go
@@ -1,9 +1,11 @@
 package actions
 
 import (
+	"culture/cache"
 	"culture/models"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gobuffalo/buffalo"
 )
@@ -20,25 +22,30 @@ func (v GeosResource) List(c buffalo.Context) error {
 		return c.Render(http.StatusBadRequest, Fail("查询地理位置集合错误 : %v", err))
 	}
 	geos := make([]GeoResult, 0, len(ids))
-	for _, id := range ids {
-		geo, err := models.REDIS.GeoPos("project_geo", id).Result()
-		if err != nil {
-			return c.Render(http.StatusBadRequest, Fail("查询地理位置错误 : %v", err))
+	if err := cache.Once("cache:project_geo", &geos, func() (interface{}, error) {
+		for _, id := range ids {
+			geo, err := models.REDIS.GeoPos("project_geo", id).Result()
+			if err != nil {
+				return nil, c.Render(http.StatusBadRequest, Fail("查询地理位置错误 : %v", err))
+			}
+			var longitude string
+			var latitude string
+			if len(geo) > 0 {
+				longitude = strconv.FormatFloat(geo[0].Longitude, 'f', -1, 64)
+				latitude = strconv.FormatFloat(geo[0].Latitude, 'f', -1, 64)
+			} else {
+				longitude = "0"
+				latitude = "0"
+			}
+			geos = append(geos, GeoResult{
+				Longitude: longitude,
+				Latitude:  latitude,
+				ID:        id,
+			})
 		}
-		var longitude string
-		var latitude string
-		if len(geo) > 0 {
-			longitude = strconv.FormatFloat(geo[0].Longitude, 'f', -1, 64)
-			latitude = strconv.FormatFloat(geo[0].Latitude, 'f', -1, 64)
-		} else {
-			longitude = "0"
-			latitude = "0"
-		}
-		geos = append(geos, GeoResult{
-			Longitude: longitude,
-			Latitude:  latitude,
-			ID:        id,
-		})
+		return geos, nil
+	}, time.Hour); err != nil {
+		return c.Render(http.StatusBadRequest, Fail("加载缓存数据失败 %v", err))
 	}
 	return c.Render(http.StatusOK, r.JSON(geos))
 }
@@ -46,6 +53,7 @@ func (v GeosResource) List(c buffalo.Context) error {
 //GeoResult GeoResult
 type GeoResult struct {
 	ID        string `json:"id"`
+	Type      string `json:"type"`
 	Longitude string `json:"longitude"`
 	Latitude  string `json:"latitude"`
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module culture
 go 1.13
 
 require (
-	cloud.google.com/go v0.36.0 // indirect
 	github.com/alcalbg/buffalo-rate-limiter-mw v0.0.0-20200129135003-ee68cfbcec5f
 	github.com/cockroachdb/cockroach-go v0.0.0-20200312223839-f565e4789405 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
@@ -30,7 +29,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/common v0.4.0
 	github.com/rs/cors v1.7.0
-	github.com/spf13/afero v1.2.1 // indirect
 	github.com/spf13/cobra v0.0.7 // indirect
 	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
